be14fafd542553f04ab1ee6fb2e28af6a783ab18
moneymoveusa01-dotcom <moneymoveusa01@gmail.com>
2026-06-25 21:30:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 const GROQ_API_KEY = process.env.GROQ_API_KEY;
 
